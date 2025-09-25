cc8f6e37ba765e75cfe2947742d6b652b824c095
OidaTiftla <oidatiftla@oidatiftla.de>
2025-09-25 15:56:07 +0000

    feat: :sparkles: add export for templates API module

--- mod.ts
+++ mod.ts
@@ -1,3 +1,25 @@
 export * from "./src/api/client.ts";
 export * from "./src/api/members.ts";
 export * from "./src/api/groups.ts";
+export * from "./src/api/templates.ts";
+
+export * from "./src/api/templates_mails.ts";
+export * from "./src/api/templates_mails_base.ts";
+
+export * from "./src/api/templates_mails_members.ts";
+export * from "./src/api/templates_mails_events.ts";
+export * from "./src/api/templates_mails_votes.ts";
+export * from "./src/api/templates_mails_conventions.ts";
+export * from "./src/api/templates_mails_shop.ts";
+export * from "./src/api/templates_mails_accounting.ts";
+export * from "./src/api/templates_mails_reservations.ts";
+export * from "./src/api/templates_mails_forum.ts";
+export * from "./src/api/templates_mails_tasks.ts";
+export * from "./src/api/templates_mails_files.ts";
+export * from "./src/api/templates_mails_blog.ts";
+export * from "./src/api/templates_mails_doubleOptIn.ts";
+export * from "./src/api/templates_mails_layout.ts";
+
+export * from "./src/api/templates_pdfs.ts";
+export * from "./src/api/templates_htmls.ts";
+export * from "./src/api/templates_newsletters.ts";
